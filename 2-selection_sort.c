8ea815ecfc17e2be2b8db8156f2db7305a30d930
01Eddie <jesus.11.97@hotmail.com>
2021-06-10 23:43:47 -0500

    [ADD] 2-selection_sort.c 2-O sort.h

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap - Function swap
+ * @xp: pointer listint_t
+ * @yp: pointer listint_t
+ * Return: void
+ */
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
+ * selection_sort - Entry point
+ * @array: pointer of a integer
+ * @size: size_t
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+			{
+				min_idx = j;
+			}
+		}
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
+	}
+}
+
