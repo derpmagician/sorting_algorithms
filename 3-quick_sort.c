723a0ba914e91c32a904259dc04d7aa88b526f58
01Eddie <jesus.11.97@hotmail.com>
2021-06-11 16:39:42 -0500

    [fixed] 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,7 @@
 #include "sort.h"
+void quick(int *array, size_t size, size_t low, size_t high);
+int partition(int *array, size_t size, size_t low, size_t high);
+void swap(int *a, int *b);
 
 /**
  * quick_sort - Entry point
@@ -11,27 +14,42 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL)
 		return;
 
-	quick(array, 0, size);
+	quick(array, size, 0, size - 1);
 }
 
-void quick(int *array, size_t low, size_t high)
+/**
+ * quick - Entry point
+ * @array: pointer integer
+ * @low: size_t
+ * @high: size_t
+ * Return: void
+ */
+
+void quick(int *array, size_t size, size_t low, size_t high)
 {
 	size_t p; //Partition index
 
 	if (low < high)
 	{
-		p = partition(array, low, high);
-		quick(array, low, p - 1);
-		quick(array, p + 1, high);
-		//print_array(array, size);
+		p = partition(array, size, low, high);
+		quick(array, size, low, p - 1);
+		quick(array, size, p + 1, high);
 	}
 }
 
+/**
+ * partition - Entry point
+ * @array: pointer integer
+ * @low: size_t
+ * @high: size_t
+ * Return: i + 1
+ */
 
-int partition(int *array, int low, int high)
+int partition(int *array, size_t size, size_t low, size_t high)
 {
-	int pivot, j;
-	int i = (low - 1);
+	int pivot;
+	size_t j;
+	size_t i = (low - 1);
 	pivot = array[high];
 
 	for (j = high; j < high; j++)
@@ -40,9 +58,23 @@ int partition(int *array, int low, int high)
 		{
 			i++;
 			swap(&array[i], &array[j]);
+			print_array(array, size);
 		}
 	}
 	swap(&array[i + 1], &array[high]);
+	print_array(array, size);
 	return (i + 1);
 }
 
+/**
+ * swap - Function swap
+ * @xp: pointer listint_t
+ * @yp: pointer listint_t
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
