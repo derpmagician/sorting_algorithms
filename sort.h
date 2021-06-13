913af432865d05f1fd3813d7042d5f860443b93d
derpmagician <renzin@gmail.com>
2021-06-13 16:38:20 -0500

    deleting unused prototype

--- sort.h
+++ sort.h
@@ -35,7 +35,4 @@ void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-/* Extra functions */
-void swap(int *xp, int *yp);
-
 #endif
