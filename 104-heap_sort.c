ccf70d851569bdf7d512c7829ebb480ba63707cc
derpmagician <renzin@gmail.com>
2021-06-15 10:32:37 -0500

    fixing function

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,99 +1,51 @@
 #include "sort.h"
+/**
+  * heapify - heap sort algorithm
+  * @idx - left index
+  * @idx2 - right index
+  * @array: array to sort
+  * @size: size of array
+  * Return: void
+  */
+void heapify(int *array, int idx, int idx2, size_t size)
+{
+	int max = idx2;
+	int left = 2 * idx2 + 1;
+	int right = 2 * idx2 + 2;
+	int tmp;
+
+	if (left < idx && array[left] > array[max])
+		max = left;
+	if (right < idx && array[right] > array[max])
+		max = right;
+	if (max != idx2)
+	{
+		tmp = array[idx2];
+		array[idx2] = array[max];
+		array[max] = tmp;
+		print_array(array, size);
+		heapify(array, idx, max, size);
+	}
+}
 
 /**
- * heap_sort - The main function to sort an array of given size
- * @array: pointer integer
- * @size: size_t
- * Return: void
- */
+  * heap_sort - heap sort algorithm
+  * @array: array to sort
+  * @size: size of array
+  * Return: void
+  */
 void heap_sort(int *array, size_t size)
 {
-	/*  Build a heap from the input data. */
-	MaxHeap_t *maxHeap = createAndBuildHeap(array, size);
+	int i, tmp;
 
-	/**
-	 * Repeat following steps while heap size is greater than 1.
-	 * The last element in max heap will be the minimum element
-	 */
-	while (maxHeap->size > 1)
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, i, size, size);
+	for (i = size - 1; i >= 0; i--)
 	{
-		/**
-		 * The largest item in Heap is stored at the root. Replace it
-		 * with the last item of the heap followed by reducing the
-		 * size of heap by 1.
-		 */
-		swap(&maxHeap->array[0], &maxHeap->array[maxHeap->size - 1]);
-		--maxHeap->size;  /*  Reduce heap size */
-
+		tmp = array[0];
+		array[0] = array[i];
+		array[i] = tmp;
 		print_array(array, size);
-		/*  Finally, heapify the root of tree. */
-		maxHeapify(maxHeap, 0, array, size);
+		heapify(array, i, 0, size);
 	}
-}
-
-/**
- * maxHeapify - Function to heapify a Max Heap.
- * @maxHeap: pointer of MaxHeap_t
- * @idx: integer
- * @array: pointer integer
- * @size: size_t
- * Return: void
- * The function assumes that everything under given root
- * (element at index idx) is already heapified
- */
-void maxHeapify(MaxHeap_t *maxHeap, int idx, int *array, size_t size)
-{
-	int largest = idx;  /* Initialize largest as root */
-	int left = (idx << 1) + 1;  /* left = 2*idx + 1 */
-	int right = (idx + 1) << 1; /* right = 2*idx + 2 */
-	/* See if left child of root exists and is greater than root*/
-	if (left < maxHeap->size && maxHeap->array[left] > maxHeap->array[largest])
-		largest = left;
-	/* See if right child of root exists and is greater than the largest so far*/
-	if (right < maxHeap->size && maxHeap->array[right] > maxHeap->array[largest])
-		largest = right;
-
-	/* Change root, if needed */
-	if (largest != idx)
-	{
-		swap(&maxHeap->array[largest], &maxHeap->array[idx]);
-		print_array(array, size);
-		maxHeapify(maxHeap, largest, array, size);
-	}
-}
-
-/**
- * createAndBuildHeap - Utility function to create a max heap of given capacity
- * @array: pointer integer
- * @size: integer
- * Return: maxHeap
- */
-MaxHeap_t *createAndBuildHeap(int *array, int size)
-{
-	int i;
-	MaxHeap_t *maxHeap = (MaxHeap_t *) malloc(sizeof(MaxHeap_t));
-
-	maxHeap->size = size;   /* initialize size of heap */
-	maxHeap->array = array; /* Assign address of first element of array */
-
-	/**
-	* Start from bottommost and rightmost internal mode and heapify all internal
-	* modes in bottom up way
-	*/
-	for (i = (maxHeap->size - 2) / 2; i >= 0; --i)
-		maxHeapify(maxHeap, i, array, size);
-	return (maxHeap);
-}
-
-/**
- * swap - A utility function to swap to integers
- * @a: pointer integer
- * @b: pointer integer
- * Return: void
- */
-void swap(int *a, int *b)
-{
-	int t = *a;
-	*a = *b;
-	*b = t;
 }
