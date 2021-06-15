0b2f50406a89e104e48140bf498945a5f984d58c
derpmagician <renzin@gmail.com>
2021-06-15 10:46:27 -0500

    fixing function

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,51 +1,70 @@
 #include "sort.h"
-/**
-  * heapify - heap sort algorithm
-  * @idx - left index
-  * @idx2 - right index
-  * @array: array to sort
-  * @size: size of array
-  * Return: void
-  */
-void heapify(int *array, int idx, int idx2, size_t size)
-{
-	int max = idx2;
-	int left = 2 * idx2 + 1;
-	int right = 2 * idx2 + 2;
-	int tmp;
 
-	if (left < idx && array[left] > array[max])
-		max = left;
-	if (right < idx && array[right] > array[max])
-		max = right;
-	if (max != idx2)
+/**
+ * swap - A utility function to swap to integers
+ * @a: pointer integer
+ * @b: pointer integer
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * heapify - build a heap tree where the head is bigger of value branches
+ * @array: unsorted array of integer
+ * @size: size of the array
+ * @i: index of starter head
+ * @print_size: size of the array to print
+ *
+ * Return: void
+ */
+void heapify(int *array, size_t size, int i, size_t print_size)
+{
+	int head = i;
+	int id_l = 2 * i + 1;
+	int id_r = 2 * i + 2;
+
+	if (id_l < (int)size && array[id_l] > array[head])
+		head = id_l;
+	if (id_r < (int)size && array[id_r] > array[head])
+		head = id_r;
+	if (head != i)
 	{
-		tmp = array[idx2];
-		array[idx2] = array[max];
-		array[max] = tmp;
-		print_array(array, size);
-		heapify(array, idx, max, size);
+		swap(&array[i], &array[head]);
+		print_array(array, print_size);
+		heapify(array, size, head, print_size);
 	}
 }
 
 /**
-  * heap_sort - heap sort algorithm
-  * @array: array to sort
-  * @size: size of array
-  * Return: void
-  */
+ * heap_sort - The main function to sort an array of given size
+ * @array: pointer integer
+ * @size: size_t
+ * Return: void
+ */
 void heap_sort(int *array, size_t size)
 {
-	int i, tmp;
+	int idx, i;
+	size_t print_size = size;
 
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, i, size, size);
+	if (size <= 1 || !array)
+		return;
+
+	idx = (int)(size / 2 - 1);
+	for (i = idx; i >= 0; i--)
+		heapify(array, size, i, print_size);
+
 	for (i = size - 1; i >= 0; i--)
 	{
-		tmp = array[0];
-		array[0] = array[i];
-		array[i] = tmp;
-		print_array(array, size);
-		heapify(array, i, 0, size);
+		if (i != 0)
+		{
+			swap(&array[0], &array[i]);
+			print_array(array, print_size);
+			heapify(array, i, 0, print_size);
+		}
 	}
 }
