a8d62a3f5dc23692c292f21048f696d828b738ec
derpmagician <renzin@gmail.com>
2021-06-16 10:03:03 -0500

    adding ex 12

---
+++ 1000-sort_deck.c
@@ -0,0 +1,122 @@
+#include "deck.h"
+
+#include <stdio.h>
+
+/**
+ * cardswap - swaps 2 consecutive cards of a doubly linked list
+ * Used in the insertion algorithm
+ * @deck: Head node for the deck
+ * @card1: The first card to cardswap
+ * @card2: The second card to cardswap
+ */
+void cardswap(deck_node_t **deck, deck_node_t *card1, deck_node_t *card2)
+{
+	deck_node_t *prev, *next;
+
+	prev = card1->prev;
+	next = card2->next;
+
+	if (prev != NULL)
+		prev->next = card2;
+	else
+		*deck = card2;
+	card1->prev = card2;
+	card1->next = next;
+	card2->prev = prev;
+	card2->next = card1;
+	if (next)
+		next->prev = card1;
+}
+
+/**
+ * vcmp - compares 2 card values
+ * @card1: The first card to compare
+ * @card2: The second card to compare
+ *
+ * Return: 1 if less than, 0 if greater or equal to
+ */
+int vcmp(const card_t *card1, const card_t *card2)
+{
+	char char1 = card1->value[0], char2 = card2->value[0];
+	const char *ord[14] = {"Ace", "1", "2", "3", "4", "5", "6",
+			       "7", "8", "9", "10", "Jack", "Queen", "King"};
+	int idx1 = 0, idx2 = 0, i;
+
+	if ((char1 >= 48 && char1 <= 57) && (char2 >= 49 &&
+					     char2 <= 57))
+	{
+		if (card1->value[1] == '0')
+			char1 = 58;
+		if (card2->value[1] == '0')
+			char2 = 58;
+		return (char1 < char2);
+	}
+	else
+	{
+		for (i = 0; i < 14; i++)
+		{
+			if (card1->value[0] == ord[i][0] &&
+			    card1->value[1] == ord[i][1])
+				idx1 = i;
+			if (card2->value[0] == ord[i][0] &&
+			    card2->value[1] == ord[i][1])
+				idx2 = i;
+		}
+		return (idx1 < idx2);
+	}
+}
+
+/**
+ * sort_suit - sorts a card deck by suit
+ * @deck: deck to sort
+ */
+void sort_suit(deck_node_t **deck)
+{
+	deck_node_t *forw, *tmp;
+
+	for (forw = (*deck)->next; forw && forw->prev; forw = forw->next)
+	{
+		for (; forw && forw->prev && forw->card->kind <
+			     forw->prev->card->kind; forw = forw->prev)
+		{
+			tmp = forw->prev;
+			cardswap(deck, tmp, forw);
+			forw = forw->next;
+		}
+	}
+}
+
+/**
+ * sort_val - sorts a card deck staticly by value
+ * @deck: deck to sort
+ */
+void sort_val(deck_node_t **deck)
+{
+	deck_node_t *f, *tmp;
+
+	for (f = (*deck)->next; f && f->prev; f = f->next)
+	{
+		for (; f && f->prev && vcmp(f->card, f->prev->card) &&
+			     f->card->kind == f->prev->card->kind;
+		     f = f->prev)
+		{
+			tmp = f->prev;
+			cardswap(deck, tmp, f);
+			f = f->next;
+		}
+	}
+}
+
+/**
+ * sort_deck - sorts a 52 card deck
+ * @deck: deck to sort
+ *
+ * Return: Always 0 (ok)
+ */
+void sort_deck(deck_node_t **deck)
+{
+	if (deck == NULL || *deck == NULL)
+		return;
+	sort_suit(deck);
+	sort_val(deck);
+}
