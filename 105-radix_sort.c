2d4ce0b5e11999e5960321e4119ad0d81f23655f
derpmagician <renzin@gmail.com>
2021-06-16 23:13:20 -0500

    fixing function

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,76 +1,50 @@
 #include "sort.h"
 
 /**
- * radix_sort - sort array with radix method
+ * radix_sort - sorts array Radix sort algorithm
  * @array: array to sort
- * @size: size of the array
- *
- * Return: nothing
-*/
-
+ * @size: array's size
+ * Return: Nothing.
+ */
 void radix_sort(int *array, size_t size)
 {
-	int i, j, x, y, cantRep, max, div = 1, t = 0;
-	int buckets[10][1000];
+	size_t i, j;
+	int *newarray = NULL, k, d, count = 1, num = 0, c = 0;
 
-	if (!array || size < 2)
+	if (!array || !size)
 		return;
-
-	max = array[0];
-	for (i = 1; i < (int) size; i++)
-		if (array[i] > max)
-			max = array[i];
-
-	for (i = 0; i < 10; ++i)
-		for (j = 0; j < 1000; ++j)
-			buckets[i][j] = -1;
-
-	cantRep = getCantRep(max);
-
-	for (i = 0; i < cantRep; ++i)
+	for (j = 0; j < size; j++)
 	{
-		for (j = 0; j < (int) size; ++j)
-		{
-			for (y = 0; buckets[(array[j] / div) % 10][y] != -1; y++)
-				;
-			buckets[(array[j] / div) % 10][y] = array[j];
-		}
-		div = div * 10;
-		t = 0;
-		for (x = 0; x < 10; ++x)
-		{
-			for (y = 0; buckets[x][y] != -1; y++)
+		if (num < array[j])
+			num = array[j];	}
+	while (num / 10 > 0)
+	{ num = num / 10;
+		count++; }
+	newarray = calloc(size, sizeof(int));
+	for (d = 0; d < count; d++)
+	{ c = 0, k = 0;
+		for (i = 0; i < size ; i++)
+		{ k = i;
+			for (j = 0; j < size ; j++)
 			{
-				array[t] = buckets[x][y];
-				buckets[x][y] = -1;
-				t++;
-			}
-		}
-		print_array(array, size);
-	}
-}
-
-/**
- * getCantRep - Returns the number of digits of the largest number in the array
- * @num: The largest number
- *
- * Return: Number of digits of the num
-*/
-
-int getCantRep(int num)
-{
-	bool flag = true;
-	int cont = 0;
-
-	while (flag)
-	{
-		flag = false;
-		if (num > 0)
-		{
-			num = num / 10;
-			cont++;
-			flag = true;
-		}
-	}
-	return (cont);
-}
+				if (d == 0)
+				{
+					if (array[j] % 10 == k)
+					{
+						newarray[c] = array[j];
+						c++; } }
+				if (d == 1)
+				{
+					if (array[j] / 10 == k)
+					{
+						newarray[c] = array[j];
+						c++; } }
+				if (d == 2)
+				{
+					if (array[j] / 100 == k)
+					{ newarray[c] = array[j];
+						c++; } } } }
+		for (i = 0; i < size ; i++)
+			array[i] = newarray[i];
+		print_array(newarray, size); }
+	free(newarray); }
