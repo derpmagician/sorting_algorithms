e4bbada8e669c8acb5838f510c4369f3386ae0e4
derpmagician <renzin@gmail.com>
2021-06-16 22:15:19 -0500

    fixing function

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,87 +1,50 @@
 #include "sort.h"
 
 /**
- * getMax - A utility function to get maximum value in arr[]
- * @array: Pointer integer
- * @n: integer
- * Return: mx
- */
-int getMax(int *array, int n)
-{
-	int i;
-	int mx = array[0];
-
-	for (i = 1; i < n; i++)
-	{
-		if (array[i] > mx)
-			mx = array[i];
-	}
-	return (mx);
-}
-
-/**
- * countSort - A function to do counting sort of array[]
- * @array: Pointer integer
- * @size: Size_t
- * @exp: integer
- * Return: void
- *  according to the digit represented by exp.
- */
-void countSort(int *array, int size, int exp)
-{
-	int output[size]; /*Output array*/
-	int i, count[10] = { 0 };
-	/*Store count of occurrence in count[] */
-	for (i = 0; i < size; i++)
-		count[(array[i] / exp) % 10]++;
-
-	/**
-	* Change count[i] so that count[i] now contains actual
-	* position of this digit in output[]
-	*/
-	for (i = 1; i < 10; i++)
-		count[i] += count[i - 1];
-
-	/* Build the output array */
-	for (i = size - 1; i >= 0; i--)
-	{
-		output[count[(array[i] / exp) % 10] - 1] = array[i];
-		count[(array[i] / exp) % 10]--;
-	}
-
-	/**
-	* Copy the output array to array[], so that array[] now contains
-	* sorted numbers according to current digit
-	*/
-	for (i = 0; i < size; i++)
-		array[i] = output[i];
-}
-
-/**
- * radix_sort - Main function to that sorts arr[] of size n using Radix Sort
- * @array: Pointer integer
- * @size: Size_t
- * Return: void
+ * radix_sort - sorts array Radix sort algorithm
+ * @array: array to sort
+ * @size: array's size
+ * Return: Nothing.
  */
 void radix_sort(int *array, size_t size)
 {
-	int exp;
-	int max;
-	int *sz;
+	size_t i, j;
+	int *newarray = NULL, k, d, count = 1, num = 0, c = 0;
 
-	sz = (malloc(sizeof(int) * size));
-	max = getMax(array, size);
-
-	if (!array || size < 2)
+	if (!array || !size)
 		return;
-
-	if (!sz)
-		return;
-
-	for (exp = 1; max / exp > 0; exp *= 10)
+	for (j = 0; j < size; j++)
 	{
-		countSort(array, size, exp);
-		print_array(array, size);
-	}
-	free(sz);
-}
+		if (num < array[j])
+			num = array[j];	}
+	while (num / 10 > 0)
+	{ num = num / 10;
+		count++; }
+	newarray = calloc(size, sizeof(int));
+	for (d = 0; d < count; d++)
+	{ c = 0, k = 0;
+		for (i = 0; i < size ; i++)
+		{ k = i;
+			for (j = 0; j < size ; j++)
+			{
+				if (d == 0)
+				{
+					if (array[j] % 10 == k)
+					{
+						newarray[c] = array[j];
+						c++; } }
+				if (d == 1)
+				{
+					if (array[j] / 10 == k)
+					{
+						newarray[c] = array[j];
+						c++; } }
+				if (d == 2)
+				{
+					if (array[j] / 100 == k)
+					{ newarray[c] = array[j];
+						c++; } } } }
+		for (i = 0; i < size ; i++)
+			array[i] = newarray[i];
+		print_array(newarray, size); }
+	free(newarray); }
